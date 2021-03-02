89c94e07308d04d2f0c9465f7635e56512480eb6
Freddie-Pike <freddie@colabsoftware.com>
2021-03-02 10:41:51 -0330

    Removing old rest route.

--- test/handlers.js
+++ test/handlers.js
@@ -1,17 +1,6 @@
-import { rest, graphql } from "msw";
+import { graphql } from "msw";
 
 const handlers = [
-  rest.get("http://localhost:3002/users/all", (_req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        users: [
-          { id: 1, name: "Rivers", email: "rivers@rivers.com" },
-          { id: 2, name: "Amy", email: "Amy@amy.com" },
-        ],
-      })
-    );
-  }),
   graphql.query("GetUserInfo", (_req, res, ctx) => {
     console.log("In GetUserInfo");
     return res(
